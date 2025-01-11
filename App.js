14933b8bc6a357c15aac36815f76e50801702a1d
kumar thapa <73516008+kumarthapa@users.noreply.github.com>
2025-01-11 12:57:57 +0530

    Revert "Update App.js"
    This reverts commit 46680506aa9f671ae7489bf7792f9b69eec334a4.

--- App.js
+++ App.js
@@ -17,7 +17,6 @@ export default function App() {
           loading={
             // eslint-disable-next-line react/jsx-wrap-multilines
             <View style={styles.container}>
-              <Text>helooo</Text>
               <ActivityIndicator color={colors.red} />
             </View>
           }
